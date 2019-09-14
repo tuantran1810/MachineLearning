3f63b06b6157a6466b34aef351c5572cc9d14db6
TuanTranEngineer <TuanTranEngineer@github.com>
2019-09-15 02:32:02 +0700

    update for linear regression

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
-
-
-# In[ ]:
-
+import BasisFunction as bf
 
 class LinearRegressor:
-    def __init__(self, X, t):
-        self.X = X
+    def __init__(self, basisFuncs, Xorig, t):
+        self.Xorig = Xorig
         self.t = t
         self.w = None
+        self.yPred = None
+        self.basisFuncs = basisFuncs
         
     def fit(self):
-        self.w = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.t)
+        X = bf.BaseBasicFunction(self.Xorig, self.basisFuncs).generate()
+        self.w = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(self.t)
         return self
     
     def predict(self, Xpred):
-        return Xpred.dot(self.w)
+        X = bf.BaseBasicFunction(Xpred, self.basisFuncs).generate()
+        self.yPred = X.dot(self.w)
+        return self.yPred
 
+    def meanSquareError(self, tPred):
+        N = float(len(tPred.ravel()))
+        e = np.subtract(tPred, self.yPred)
+        return np.asscalar((e.T.dot(e)/N).ravel())
+        
+class RidgeLinearRegressor(LinearRegressor):
+    def __init__(self, basisFuncs, Xorig, t, lamb = 0):
+        self.lamb = lamb
+        super().__init__(basisFuncs, Xorig, t)
+        
+    def fit(self):
+        X = bf.BaseBasicFunction(self.Xorig, self.basisFuncs).generate()
+        lamb = self.lamb * np.identity(len(X.T))
+        tmp = np.linalg.inv(np.add(lamb, X.T.dot(X)))
+        self.w = tmp.dot(X.T).dot(self.t)
+        return self
+    
