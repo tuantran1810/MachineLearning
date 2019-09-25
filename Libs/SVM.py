83277eca0db1841e2d1237817cd1e47cab30d6c3
TuanTranEngineer <TuanTranEngineer@github.com>
2019-09-25 08:45:49 +0700

    update for svm

--- Libs/SVM.py
+++ Libs/SVM.py
@@ -1,10 +1,76 @@
 import numpy as np
-import CVXOPT
+from cvxopt import matrix, solvers
 
 class SVM():
-    def __init__(self, X, t):
-        self.X = X
-        self
+    def __init__(self, Xorig, torig):
+        self.Xorig = Xorig
+        self.torig = torig
+        self.N = len(torig)
+        self.Korig = len(Xorig[0])
+        self.w = None
 
-class PrimalSVM():
-    self.x
+    def fit(self):
+        raise Exception("fit() haven't been implemented!")
+
+    def __classify(self, tpredict):
+        for t in tpredict.ravel():
+            if t > 0.0:
+                yield 1
+            else:
+                yield 0
+
+    def predict(self, Xpredict, raw = False):
+        if self.w is None:
+            raise Exception("w haven't been calculated, do fit() first")
+        tpredict = Xpredict.dot(self.w)
+        if raw: return tpredict
+        return np.array([i for i in self.__classify(tpredict)], dtype = int).reshape(-1, 1)
+
+class PrimalSVM(SVM):
+    def __init__(self, X, t):
+        N = len(X)
+        one = np.ones(N, dtype = float).reshape(-1, 1)
+        self.X = np.hstack((X, one))
+
+        K = len(self.X[0])
+        tfinal = t[:, :]
+        for _ in range(K - 1):
+            tfinal = np.hstack((tfinal, t))
+        self.t = tfinal
+        self.K = K
+        super().__init__(X, t)
+
+    def fit(self):
+        K = np.identity(self.K, dtype = float)
+        K[self.K - 1][self.K - 1] = 0.0
+        K = matrix(K)
+        p = matrix(np.zeros(self.K, dtype = float).reshape(-1, 1))
+        G = matrix(-(self.X * self.t))
+        h = -np.ones(self.N, dtype = float).reshape(-1, 1)
+        h = matrix(h)
+
+        solvers.options['show_progress'] = False
+        solultion = solvers.qp(K ,p ,G , h)
+        self.w = np.array(solultion['x']).reshape(-1, 1)
+        print("done fitting, w = \n{}".format(self.w))
+        return self
+
+    def raw_predict(self, Xpredict):
+        N = len(Xpredict)
+        Xpredict = np.hstack((Xpredict, np.ones(N, dtype = float).reshape(-1, 1)))
+        return super().predict(Xpredict, True)
+
+    def predict(self, Xpredict):
+        N = len(Xpredict)
+        Xpredict = np.hstack((Xpredict, np.ones(N, dtype = float).reshape(-1, 1)))
+        return super().predict(Xpredict)
+
+    def __findSupportVectorPoints(self):
+        ypredict = super().predict(self.X, True)
+        for i in range(len(ypredict)):
+            num = np.asscalar(ypredict[i])
+            if (num > -1.001 and num < -0.999) or (num < 1.001 and num > 0.999):
+                yield self.X[i][0:-1]
+
+    def supportVectorPoints(self):
+        return np.array([point for point in self.__findSupportVectorPoints()])
