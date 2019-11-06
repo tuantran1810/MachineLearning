b6a0777388225905709c4f4d0f128924211ea01f
Tuấn Trần <tuantran@MacBook-Pro-4.local>
2019-11-06 13:43:59 +0700

    hmm update

--- Libs/HMM.py
+++ Libs/HMM.py
@@ -10,11 +10,11 @@ class HMMProbCalculator():
             raise Exception("A, B, pi are not identical")
         self.NStates = len(pi)
         self.ObservationRange = len(B[0])
-        # self.AGenerator = [x for ai in A: x = ProbDistributor.Distributor(ai)]
-        # self.BGenerator = [x for bi in B: x = ProbDistributor.Distributor(bi)]
-        # self.piGenerator = ProbDistributor.Distributor(pi)
         self.NObservation = 0
         self.alpha = None
+
+    def __str__(self):
+        return str(self.alpha)
 
     def _fitInit(self, observation):
         self.NObservation = len(observation)
@@ -22,6 +22,7 @@ class HMMProbCalculator():
         firstObs = observation[0]
         self.alpha = np.zeros(shape = (self.NStates, self.NObservation))
         self.alpha[:, 0] = self.pi*self.B[:, firstObs]
+        return True
 
     def _fitInduction(self, observation):
         for i in range(1, self.NObservation):
@@ -33,9 +34,53 @@ class HMMProbCalculator():
     def fit(self, observation):
         for obs in observation:
             if obs >= self.NStates: raise Exception("observation state invalid")
-        self._fitInit(observation)
+        if not self._fitInit(observation):
+            raise Exception("Can't initialize HMM Probability Calculator for the observation")
         self._fitInduction(observation)
+        return self
+
+    def getFittingProb(self):
+        if self.alpha is None: raise Exception("model haven't been fitted")
         return np.sum(self.alpha[:, self.NObservation - 1])
+
+class HMMViterbiStatePredictor(HMMProbCalculator):
+    def __init__(self, A, B, pi):
+        super().__init__(A, B, pi)
+        self.theta = None
+
+    def __str__(self):
+        alphaStr = str(self.alpha)
+        thetaStr = str(self.theta)
+        return "alpha:\n" + alphaStr + "\ntheta:\n" + thetaStr
+
+    def _fitInit(self, observation):
+        if not super()._fitInit(observation): return False
+        self.theta = np.zeros(shape = (self.NStates, self.NObservation))
+        return True
+
+    def _fitInduction(self, observation):
+        for i in range(1, self.NObservation):
+            preProb = self.alpha[:, i - 1].reshape(-1, 1)
+            tmpProb = preProb * self.A
+            postProb = np.max(tmpProb, axis = 0).reshape(-1, 1)
+            obsProb = self.B[:, observation[i]].reshape(-1, 1)
+            self.alpha[:, i] = (postProb * obsProb).flatten()
+            self.theta[:, i] = np.argmax(tmpProb, axis = 0).flatten()
+
+    def getFittingProb(self):
+        if self.alpha is None: raise Exception("model haven't been fitted")
+        return np.max((self.alpha[:, self.NObservation - 1]).ravel())
+
+    def getPath(self):
+        if self.alpha is None or self.theta is None: raise Exception("model haven't been fitted")
+        endPointIndex = np.argmax((self.alpha[:, self.NObservation - 1]).ravel())
+        lst = [int(self.theta[endPointIndex, self.NObservation - 1])]
+        for i in range(self.NObservation - 2, -1, -1):
+            endPointIndex = lst[-1]
+            lst.append(int(self.theta[endPointIndex, i]))
+        reversed(lst)
+        return lst
+
 
 A = np.array(
     [
@@ -55,5 +100,11 @@ B = np.array(
 
 pi = np.array([0.25, 0.25, 0.25, 0.25])
 
-cal = HMMProbCalculator(A, B, pi).fit([0, 2, 1, 1])
+cal = HMMProbCalculator(A, B, pi).fit([0, 2, 1, 1, 1, 0, 0, 2, 2])
 print(cal)
+print(cal.getFittingProb())
+
+viterbi = HMMViterbiStatePredictor(A, B, pi).fit([0, 2, 1, 1, 1, 0, 0, 2, 2])
+print(viterbi)
+print(viterbi.getFittingProb())
+print(viterbi.getPath())
